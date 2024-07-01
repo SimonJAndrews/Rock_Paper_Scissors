89735b80a330afe571ef9a2182c0bc8e782af733
Simon Andrews <simon.joseph.andrews@icloud.com>
2024-07-01 22:55:20 +0100

    comitting changes to Javascript

--- main.js
+++ main.js
@@ -4,18 +4,16 @@ let options = ["rock", "paper", "scissors"];
 //this function generates a random number and rounds it down to a whiole number//
 function getComputerChoice(){
   const choice = options[Math.floor(Math.random() * options.length)];
-  console.log(choice)
   return choice;
 }
 
 // this function checks who is the winner and returns the answer. it takes two arguments.
 function checkWinner(playerSelection, computerSelection){
-  if(playerSelection == computerSelection){
-  return "its a Tie.";
-
-  }else if((playerSelection == "rock" && computerSelection == "scissors") || 
-  (playerSelection == "paper" && computerSelection == "rock") ||
-   (playerSelection =="scissors" && computerSelection == "paper")){
+  if(playerSelection === computerSelection){
+  return "Tie";
+  }else if((playerSelection === "rock" && computerSelection === "scissors") || 
+  (playerSelection === "paper" && computerSelection === "rock") ||
+   (playerSelection === "scissors" && computerSelection === "paper")){
     return "Player";
   }else{
     return "Computer"
@@ -24,7 +22,7 @@ function checkWinner(playerSelection, computerSelection){
 //this function checks the result of the checkWinner function above and returns a message dependant on the result.
 function playRound(playerSelection, computerSelection){
   const result = checkWinner(playerSelection,computerSelection);
-    if ( result == "its a Tie"){
+    if ( result == "Tie"){
       return "its a Tie!"
     }else if (result == "Player"){
       return `You win ${playerSelection} beats ${computerSelection}`
@@ -32,11 +30,54 @@ function playRound(playerSelection, computerSelection){
       return `You lose ${computerSelection} beats ${playerSelection}`
     }
   }
+// This function gets the players choice using input and converts it to lower case. It also makes sure the input is valid(validated.)
+  function getPlayerChoice(){
+   let validatedInput = false;
+   while(validatedInput == false){
+    const choice = prompt("Rock Paper or Scissors");
+   if(choice == null){
+      continue;
+   }
+   const choiceInLower = choice.toLowerCase();
+   if (options.includes(choiceInLower)){
+     validatedInput = true;
+     return choiceInLower;
+   }
+
+   }
+  }
+
+  // This function increments the scores of each player, tracks the round and logs the outcome messages to console.
+  function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome")
+    for (let index = 0; index < 5; index++) {
+      const playerSelection = getPlayerChoice();
+      const computerSelection = getComputerChoice();
+      console.log(playRound(playerSelection,computerSelection))
+      console.log("--------------------")
+      if(checkWinner(playerSelection , computerSelection) == "Player"){
+        scorePlayer++
+      }else if (checkWinner(playerSelection , computerSelection)== "Computer"){
+        scoreComputer++
+      }
+    }
+    console.log("Game Over")
+    if (scorePlayer > scoreComputer){
+      console.log("Congratulations You Win !")
+    }else if(scorePlayer < scoreComputer){
+      console.log(" Sorry The computer won. Better luck next time")
+    }else{
+      console.log("Its a Draw!!!")
+    }
+  }
+
+  game()
 
 
-const playerSelection ="rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+
+
 
 
 
